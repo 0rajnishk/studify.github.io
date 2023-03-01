1237daea99e9ea87c91b042b9e7299fc7081000b
0rajnishk <96879734+0rajnishk@users.noreply.github.com>
2023-03-01 15:38:43 +0530

    Update main.js
    resolved an issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,18 +29,27 @@ const sections = document.querySelectorAll('section[id]')
 function scrollActive(){
   const scrollY = window.pageYOffset
 
-  sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight
-      const sectionTop = current.offsetTop - 50;
-      sectionId = current.getAttribute('id')
+  function scrollActive() {
+  const sections = document.querySelectorAll('section');
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
 
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-      }else{
-          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      const activeLink = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+      if (activeLink) {
+        activeLink.classList.add('active-link');
       }
-  })
+    } else {
+      const inactiveLink = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+      if (inactiveLink) {
+        inactiveLink.classList.remove('active-link');
+      }
+    }
+  });
 }
+
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
